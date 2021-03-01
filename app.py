ec45aedef797ad04a69b03e26055c4d991036c07
Alex Martinez <alexmartinez1745@gmail.com>
2021-03-01 12:31:41 -0500

    welcome routes for flask

--- app.py
+++ app.py
@@ -2,5 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Flask routes
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Weather API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
 if __name__ == "__main__":
     app.run(debug=True)
